a5d6bb2365b3664136a0ee41074d17dd40463999
YizYah <yyakovson613@gmail.com>
2020-05-24 12:50:53 +0300

    Updates

---
+++ precache-manifest.4d502f6722bd2adc1107bd3385515f64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a74446cec72e1259fa147624235005",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e29697e1de921d6ae0f9",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "362433bb2ba3dbb6d06c",
+    "url": "/my-app/static/js/2.1c3e2f1d.chunk.js"
+  },
+  {
+    "revision": "dc68cda4036ba81febd62b416dc2a024",
+    "url": "/my-app/static/js/2.1c3e2f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29697e1de921d6ae0f9",
+    "url": "/my-app/static/js/main.68c601e8.chunk.js"
+  },
+  {
+    "revision": "02b56c28fd0dd5900528",
+    "url": "/my-app/static/js/runtime-main.d07d8efe.js"
+  }
+]);
